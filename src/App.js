a520b3e7f1d9ce971917baea4ab141b082c14ce0
onetrillion <tradgardsmastaren@protonmail.com>
2023-05-24 19:15:11 +0200

    Implement infinite scroll on cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import BirthdayCard from "./components/BirthdayCard.js";
 import FormModal from "./components/FormModal.js";
-import { useRecoilValue } from "recoil";
-import { birthdaysState } from "./recoil/atom/bithdayAtom";
 import { Row } from "antd";
+import CardList from "./components/CardList.js";
 
 const App = () => {
-    const birthdays = useRecoilValue(birthdaysState);
-
     return (
         <div className="App">
             <div
                 style={{
                     backgroundColor: "green",
                     padding: "20px 15px 20px 15px",
-                    marginBottom: "20px",
-                    filter: "drop-shadow(5px 5px 10px gray)",
+                    filter: "drop-shadow(5px 5px 10px black)",
+                    position: "fixed",
+                    width: "95%",
+                    zIndex: 999,
                 }}
             >
                 <Row justify={"space-between"}>
@@ -30,17 +28,7 @@ const App = () => {
                     <FormModal />
                 </Row>
             </div>
-            {birthdays.map((birthday) => {
-                return (
-                    <BirthdayCard
-                        key={birthday.id}
-                        name={birthday.name}
-                        age={birthday.age}
-                        date={birthday.date}
-                        picture={birthday.picture}
-                    />
-                );
-            })}
+            <CardList />
         </div>
     );
 };
---
+++ src/components/CardList.js
@@ -0,0 +1,77 @@
+import React, { useState, useRef, useEffect } from "react";
+import BirthdayCard from "./BirthdayCard";
+import { useRecoilValue } from "recoil";
+import { birthdaysState } from "../recoil/atom/bithdayAtom";
+
+const CardList = () => {
+    const cards = useRecoilValue(birthdaysState);
+    const [visibleCards, setVisibleCards] = useState([]);
+    const containerRef = useRef(null);
+    const prevScrollTopRef = useRef(0);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        const cardCount = cards.length;
+        let startIndex = 0;
+
+        const handleScroll = () => {
+            const scrollTop = container.scrollTop;
+            const containerHeight = container.clientHeight;
+            const scrollHeight = container.scrollHeight;
+
+            if (scrollTop > prevScrollTopRef.current) {
+                // Scrolling down
+                if (scrollTop + containerHeight >= scrollHeight) {
+                    startIndex = (startIndex + 1) % cardCount;
+                    setVisibleCards([
+                        ...cards.slice(startIndex),
+                        ...cards.slice(0, startIndex),
+                    ]);
+                }
+            } else {
+                // Scrolling up
+                if (scrollTop === 0) {
+                    startIndex = (startIndex - 1 + cardCount) % cardCount;
+                    setVisibleCards([
+                        ...cards.slice(startIndex),
+                        ...cards.slice(0, startIndex),
+                    ]);
+                    container.scrollTop =
+                        container.scrollHeight - containerHeight;
+                }
+            }
+
+            prevScrollTopRef.current = container.scrollTop;
+        };
+
+        container.addEventListener("scroll", handleScroll);
+        return () => {
+            container.removeEventListener("scroll", handleScroll);
+        };
+    }, [cards]);
+
+    useEffect(() => {
+        // Initialize visible cards when the component mounts
+        setVisibleCards(cards);
+    }, [cards]);
+
+    return (
+        <div
+            ref={containerRef}
+            style={{ height: "100vh", overflowY: "scroll" }}
+        >
+            {visibleCards.map((card) => (
+                <BirthdayCard
+                    key={card.id}
+                    name={card.name}
+                    age={card.age}
+                    date={card.date}
+                    picture={card.picture}
+                    className="birthday-card"
+                />
+            ))}
+        </div>
+    );
+};
+
+export default CardList;
