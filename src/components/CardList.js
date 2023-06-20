7157b3d4dc678da45ac3554bdaa8ee2c39f6556a
onetrillion <tradgardsmastaren@protonmail.com>
2023-06-20 18:43:11 +0200

    Remove infinite scroll cards

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,106 +1,92 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import BirthdayCard from "./BirthdayCard";
 import { useRecoilValue } from "recoil";
-import { birthdaysState } from "../recoil/atom/bithdayAtom";
+import { birthdaysState } from "../recoil/atom/birthdayAtom";
+import { v4 as uuid } from "uuid";
 
 const CardList = () => {
-    const cards = useRecoilValue(birthdaysState);
-    const [visibleCards, setVisibleCards] = useState([]);
-    const containerRef = useRef(null);
-    const prevScrollTopRef = useRef(0);
+  const cards = useRecoilValue(birthdaysState);
+  const [visibleCards, setVisibleCards] = useState([]);
 
-    const genBirthDate = (cardDate) => {
-        const monthName = cardDate.toLocaleString("en-US", {
-            month: "long",
-        });
-        return cardDate.getDate() + " " + monthName;
-    };
+  const genBirthDate = (cardDate) => {
+    const monthName = cardDate.toLocaleString("en-US", {
+      month: "long"
+    });
+    return cardDate.getDate() + " " + monthName;
+  };
 
-    const sortCardsByDate = (c) => {
-        c.sort(function (a, b) {
+  const sortCardsByDate = (c) => {
+    c.sort(function (a, b) {
+      const monthA = a.date.getMonth();
+      const dayA = a.date.getDate();
+      const monthB = b.date.getMonth();
+      const dayB = b.date.getDate();
 
-            const monthA = a.date.getMonth();
-            const dayA = a.date.getDate();
-            const monthB = b.date.getMonth();
-            const dayB = b.date.getDate();
+      if (monthA < monthB) {
+        return -1;
+      } else if (monthA > monthB) {
+        return 1;
+      } else {
+        if (dayA < dayB) {
+          return -1;
+        } else if (dayA > dayB) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    });
+    return c;
+  };
 
-            if (monthA < monthB) {
-                return -1;
-            } else if (monthA > monthB) {
-                return 1;
-            } else {
-                if (dayA < dayB) {
-                    return -1;
-                } else if (dayA > dayB) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });
-        return c;
-    };
+  useEffect(() => {
+    const sortedCards = sortCardsByDate(cards.slice());
 
-    useEffect(() => {
-        const sortedCards = sortCardsByDate(cards.slice());
-        setVisibleCards(cards);
-        const container = containerRef.current;
-        const cardCount = sortedCards.length;
-        let startIndex = 0;
+    const currentYear = new Date().getFullYear();
+    const nextYear = currentYear + 1;
 
-        const handleScroll = () => {
-            const scrollTop = container.scrollTop;
-            const containerHeight = container.clientHeight;
-            const scrollHeight = container.scrollHeight;
+    const visibleCardsWithYear = [];
 
-            if (scrollTop > prevScrollTopRef.current) {
-                // Scrolling down
-                if (scrollTop + containerHeight >= scrollHeight) {
-                    startIndex = (startIndex + 1) % cardCount;
-                    setVisibleCards([
-                        ...sortedCards.slice(startIndex),
-                        ...sortedCards.slice(0, startIndex),
-                    ]);
-                }
-            } else {
-                // Scrolling up
-                if (scrollTop === 0) {
-                    startIndex = (startIndex - 1 + cardCount) % cardCount;
-                    setVisibleCards([
-                        ...sortedCards.slice(startIndex),
-                        ...sortedCards.slice(0, startIndex),
-                    ]);
-                    container.scrollTop =
-                        container.scrollHeight - containerHeight;
-                }
-            }
+    // Add previous year's cards
+    sortedCards.forEach((card) => {
+      visibleCardsWithYear.push(card);
+    });
+    visibleCardsWithYear.push({ year: currentYear });
+    // Add current year's cards
+    sortedCards.forEach((card) => {
+      visibleCardsWithYear.push(card);
+    });
+    visibleCardsWithYear.push({ year: nextYear });
+    // Add next year's cards
+    sortedCards.forEach((card) => {
+      visibleCardsWithYear.push(card);
+    });
 
-            prevScrollTopRef.current = container.scrollTop;
-        };
+    setVisibleCards(visibleCardsWithYear);
+  }, [cards]);
 
-        container.addEventListener("scroll", handleScroll);
-        return () => {
-            container.removeEventListener("scroll", handleScroll);
-        };
-    }, [cards]);
-
-    return (
-        <div
-            ref={containerRef}
-            style={{ height: "100vh", overflowY: "scroll" }}
-        >
-            {visibleCards.map((card) => (
-                <BirthdayCard
-                    key={card.id}
-                    name={card.name}
-                    age={card.age}
-                    date={genBirthDate(card.date)}
-                    picture={card.picture}
-                    className="birthday-card"
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div style={{ height: "100vh", overflowY: "scroll" }}>
+      {visibleCards.map((card, index) => (
+        <React.Fragment key={uuid()}>
+          {card.year ? (
+            <div style={{ fontWeight: "bold", textAlign: "center", fontSize: 30 }}>
+              {card.year}
+            </div>
+          ) : (
+            <BirthdayCard
+              key={card.id || index}
+              name={card.name}
+              age={card.age}
+              date={genBirthDate(card.date)}
+              picture={card.picture}
+              className="birthday-card"
+            />
+          )}
+        </React.Fragment>
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
